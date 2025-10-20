650cb4d27163b8e8b49c4ddec79a4999d43274cc
FROComp <fabiojunio385@outlook.com>
2025-10-20 15:52:10 -0300

    feat: add user profile page with gravatar and articles display

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+
+  def gravatar_for(user = nil, options = { size: 200 })
+    user ||= @user
+    size = options[:size]
+    gravatar_id = Digest::SHA256::hexdigest(user.email.downcase)
+    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
+    image_tag(gravatar_url, alt: user.username, class: "rounded mx-auto d-block")
+  end
+
 end
