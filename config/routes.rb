743761a6a2ad93a383657a28c582bfb2024236f9
FROComp <fabiojunio385@outlook.com>
2025-10-20 13:12:15 -0300

    feat: implement user signup functionality with form and error handling

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,14 @@
 Rails.application.routes.draw do
-  resources :articles
+  # Home
   root 'pages#home'
   get 'about', to: 'pages#about'
+
+  # Article
+  resources :articles
+
+  # Signup
+  get 'signup', to: 'users#new'
+
+  # Users
+  resources :users, except: [:new]
 end
