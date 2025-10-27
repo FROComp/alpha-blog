a8e163370bdb05d98d191c20c74e965ae5d0451c
FROComp <fabiojunio385@outlook.com>
2025-10-27 14:35:13 -0300

    feat: enforce user authentication and ownership checks for article and user management

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,11 @@
 class ApplicationController < ActionController::Base
   include SessionsHelper
   helper_method :current_user, :logged_in?
+
+  def require_user
+    unless logged_in?
+      flash[:alert] = "You must be logged in to perform that action"
+      redirect_to login_path
+    end
+  end
 end
